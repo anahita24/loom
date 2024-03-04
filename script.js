14f3dc1b4ef5af9729440b2c12a76909bda1636e
Anahita Tiwary <anahitatiwary@gmail.com>
2024-03-05 00:38:06 +0530

    record capture funtionality

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let video = document.querySelector("video");
-let recordBtnContainer = document.querySelector("record-btn-cont");
-let recordBtn = document.querySelector("record-btn");
+let recordBtnContainer = document.querySelector(".record-btn-cont");
+let recordBtn = document.querySelector(".record-btn");
 
-let captureBtnContainer = document.querySelector("capture-btn-cont");
-let captureBtn = document.querySelector("capture-btn");
+let captureBtnContainer = document.querySelector(".capture-btn-cont");
+let captureBtn = document.querySelector(".capture-btn");
 let recordFlag = false
 
 
